a639d0248d6dd95126ffb86496caaeae42d7e29e
TheoVasa <79704565+TheoVasa@users.noreply.github.com>
2021-03-15 19:18:37 +0100

    4.2 version

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -1,10 +1,42 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
+
 public final class StationPartition implements StationConnectivity {
 
+    private final int[] partition;
+
+
+
+    private StationPartition(int[] partition){
+        this.partition = partition;
+}
 
     @Override
     public boolean connected(Station s1, Station s2) {
-        return false;
+
+        int s1Index = ChMap.stations().indexOf(s1);
+        int s2Index = ChMap.stations().indexOf(s2);
+
+        if(s1Index> partition.length || s2Index> partition.length)
+            return s1Index==s2Index;
+        else
+        return partition[s1Index] == partition[s2Index];
     }
+
+    public static final class Builder{
+
+        private final int[] BuilderPartition;
+
+        public Builder(int count) {
+            Preconditions.checkArgument(count<0);
+
+
+
+
+        }
+    }
+
+
+
 }
