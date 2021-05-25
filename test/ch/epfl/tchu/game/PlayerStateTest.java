8e93899945a412b262b8d3d77edc37307fde8302
TheoVasa <79704565+TheoVasa@users.noreply.github.com>
2021-05-25 10:53:07 +0200

    11.2 version

--- test/ch/epfl/tchu/game/PlayerStateTest.java
+++ test/ch/epfl/tchu/game/PlayerStateTest.java
@@ -111,7 +111,6 @@ class PlayerStateTest {
             for (int i = 0; i + batchSize < cards.size(); i += batchSize) {
                 var nextI = i + batchSize;
                 var cardsToAdd = cards.subList(i, nextI);
-                playerState = playerState.withAddedCards(SortedBag.of(cardsToAdd));
                 assertEquals(SortedBag.of(cards.subList(0, nextI)), playerState.cards());
             }
         }
