093f7f5880af4f6997442942be4a653d8ff5de78
TheoVasa <79704565+TheoVasa@users.noreply.github.com>
2021-05-25 11:02:12 +0200

    11.3 version

---
+++ test/ch/epfl/tchu/gui/Stage11Test.java
@@ -0,0 +1,27 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
+import ch.epfl.tchu.gui.GraphicalPlayerAdapter;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Map;
+import java.util.Random;
+
+public final class Stage11Test extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) {
+        SortedBag<Ticket> tickets = SortedBag.of(ChMap.tickets());
+        Map<PlayerId, String> names =
+                Map.of(PlayerId.PLAYER_1, "Ada", PlayerId.PLAYER_2, "Charles");
+        Map<PlayerId, Player> players =
+                Map.of(PlayerId.PLAYER_1, new GraphicalPlayerAdapter(),
+                        PlayerId.PLAYER_2, new GraphicalPlayerAdapter());
+        Random rng = new Random();
+        new Thread(() -> Game.play(players, names, tickets, rng))
+                .start();
+    }
+}
